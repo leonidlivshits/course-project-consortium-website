816541b8bc59561223f49f150d30f9e03f5834bb
leonidlivshits <Leolivshitsss@yandex.ru>
2024-12-25 23:29:11 +0300

    include Python.h bug fixed
    include Python.h bug fixed
    Added stubs of pages

---
+++ my-app/src/pages/Events/Events.jsx
@@ -0,0 +1,2 @@
+const Events = () => <div className="content"><h1>События</h1></div>;
+export default Events;
---
+++ my-app/src/pages/News/News.jsx
@@ -0,0 +1,2 @@
+const News = () => <div className="content"><h1>Новости</h1></div>;
+export default News;
