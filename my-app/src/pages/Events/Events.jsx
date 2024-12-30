e6df948d9d404330d46ed1635c76d892a3b1e120
Dmitry-Alekseev01 <145341589+Dmitry-Alekseev01@users.noreply.github.com>
2024-12-30 14:51:10 +0300

    Фронт продолжат

--- my-app/src/pages/Events/Events.jsx
+++ my-app/src/pages/Events/Events.jsx
@@ -1,2 +1,21 @@
-const Events = () => <div className="content"><h1>События</h1></div>;
+// const Events = () => <div className="content"><h1>События</h1></div>;
+// export default Events;
+
+import React from "react";
+
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+const Events = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="page"> 
+      <h1>События</h1>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
 export default Events;
