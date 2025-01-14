54636b635de79177e9b39ca3790f39f492d16eab
leonidlivshits <Leolivshitsss@yandex.ru>
2025-01-14 14:00:26 +0300

    Added backend for projects, events
    Added backend for projects, events
    Added python test scripts

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -24,3 +24,20 @@ class Contact(db.Model):
     phone = db.Column(db.String(20), nullable=False)
     company = db.Column(db.String(100))
     message = db.Column(db.Text, nullable=False)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    time = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)  # Название проекта
+    authors = db.Column(db.String(200), nullable=False)  # Авторы (можно хранить как строку)
+    publication_date = db.Column(db.String(50), nullable=False)  # Дата публикации
+    description = db.Column(db.Text, nullable=False)  # Описание проекта
+    content = db.Column(db.Text, nullable=False)  # Основной текст проекта
+    materials = db.Column(db.Text)  # Материалы (например, ссылки или файлы)
--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -66,8 +66,43 @@
 #     mail.send(msg)
 
 #     return jsonify({'message': 'Сообщение отправлено успешно!'}), 201
+
+
+# from flask import Blueprint, request, jsonify
+# from .models import db, Contact
+# from flask_mail import Message
+# from . import mail
+
+# main = Blueprint('main', __name__)
+
+# @main.route('/api/contact', methods=['POST'])
+# def create_contact():
+#     print("create_contact --------------------------------------")
+#     data = request.get_json()
+#     new_contact = Contact(
+#         name=data['name'],
+#         email=data['email'],
+#         phone=data['phone'],
+#         company=data.get('company'),
+#         message=data['message']
+#     )
+#     db.session.add(new_contact)
+#     db.session.commit()
+
+#     msg = Message(
+#         subject=f'Новое сообщение от {data["name"]}',
+#         sender=data['email'],
+#         recipients=['maxweinsberg25@gmail.com'],
+#         body=f"Имя: {data['name']}\nEmail: {data['email']}\nТелефон: {data['phone']}\nКомпания: {data.get('company')}\nСообщение: {data['message']}"
+#     )
+#     mail.send(msg)
+
+#     return jsonify({'message': 'Сообщение отправлено успешно!'}), 201
+
+# Последняя рабочая версия
+
 from flask import Blueprint, request, jsonify
-from .models import db, Contact
+from .models import db, Contact, Event, Project
 from flask_mail import Message
 from . import mail
 
@@ -96,3 +131,34 @@ def create_contact():
     mail.send(msg)
 
     return jsonify({'message': 'Сообщение отправлено успешно!'}), 201
+
+@main.route('/api/events', methods=['GET'])
+def get_events():
+    events = Event.query.all()
+    events_list = []
+    for event in events:
+        events_list.append({
+            'id': event.id,
+            'title': event.title,
+            'date': event.date,
+            'time': event.time,
+            'location': event.location,
+            'description': event.description
+        })
+    return jsonify(events_list), 200
+
+@main.route('/api/projects', methods=['GET'])
+def get_projects():
+    projects = Project.query.all()
+    projects_list = []
+    for project in projects:
+        projects_list.append({
+            'id': project.id,
+            'title': project.title,
+            'authors': project.authors,
+            'publication_date': project.publication_date,
+            'description': project.description,
+            'content': project.content,
+            'materials': project.materials
+        })
+    return jsonify(projects_list), 200
